1c285b7f1d20578a91893fdd57f02bce4ab61a6d
Loren <lmcrawf@gmail.com>
2015-11-11 15:18:49 -0500

    Trying to set up the twitter API

--- tweet.rb
+++ tweet.rb
@@ -3,8 +3,13 @@ class Tweet
 
 	def self.search(handler)
 		base_url = "https://api.twitter.com/1.1/search/tweets.json"
+		since_id = 24012619984051000
+		max_id = 250126199840518145
 
-		HTTParty.get ("#{base_url}?q=%40#{handler}")
+		data_response = HTTParty.get ("#{base_url}q=%23#{handler}&since_id=#{since_id}&max_id=")
+
+		tweet_text = data_response
+		sentiment_response = HTTParty.get ("https://www.tweetsentimentapi.com/api/?key=f96b60fbda0c227488d4544ff70d02297f0f775c&text= #{tweet_text}")
 	end
 
 end
