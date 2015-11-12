3d24a0897e5feab2b4f9f0ede17607cd201dc9b7
Loren <lmcrawf@gmail.com>
2015-11-12 11:15:34 -0500

    Tried to add a new array with only positive sentiment in order to count the number of positive tweets

--- tweet.rb
+++ tweet.rb
@@ -11,12 +11,17 @@ class Tweet
 
 		sentiment_response = sentiment_text.map {|text| HTTParty.get ("https://www.tweetsentimentapi.com/api/?key=f96b60fbda0c227488d4544ff70d02297f0f775c&text=#{text}")}
 
-		# print sentiment_response
+		
+		positive_tweets = []
+		if sentiment_response[0]["sentiment"] == "positive"
+			positive_tweets.push
+			puts "#{positive_tweets.count} positive tweets were found that mentioned #{search_text}. \n"
+		end 
 
-		print "#{sentiment_response.count {|response| response["sentiment"] = "positive"}} positive tweets were found that mentioned #{search_text}. \n"
+		# print "#{sentiment_response.count {|response| response["sentiment"] = "positive"}} positive tweets were found that mentioned #{search_text}. \n"
 		
 		print "twitter user  |  tweet link  | twitter message \n"
-		print search_result.parsed_response["statuses"].map {|tweet| tweet["text"]}
+		
 	end
 
 end
