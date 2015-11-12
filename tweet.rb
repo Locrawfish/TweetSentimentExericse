1012d132220b609c5c803ebb6a9735e9ad40bda4
Loren <lmcrawf@gmail.com>
2015-11-11 21:04:21 -0500

    Added the Oath key to the HTTParty get request

--- tweet.rb
+++ tweet.rb
@@ -1,12 +1,13 @@
 class Tweet 
 	require "httparty"
 
-	def self.search(handler)
-		base_url = "https://api.twitter.com/1.1/search/tweets.json"
+	def self.search(search_text)
 		
-		data_response = HTTParty.get ("#{base_url}q=%23#{handler}&since_id=#{since_id}&max_id=")
+		
+		search_result = HTTParty.get "https://api.twitter.com/1.1/search/tweets.json?q=#{search_text}", :headers =>{"Authorization"=>"Bearer AAAAAAAAAAAAAAAAAAAAALaMigAAAAAAWn5FCNWFWcMCqnB%2FPfUgmUqNzNU%3DQXOykvCCGD4picjpP0PCTrb4sW6tM78EWFRx2gZXYCL2bRNdqy"}
 
-		tweet_text = data_response
+		tweet_text = search_result
+
 		sentiment_response = HTTParty.get ("https://www.tweetsentimentapi.com/api/?key=f96b60fbda0c227488d4544ff70d02297f0f775c&text= #{tweet_text}")
 	end
 
