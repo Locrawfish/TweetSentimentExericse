9087494e7fb0242de408dc1d3b36876c77a62edd
Loren <lmcrawf@gmail.com>
2015-11-12 11:03:29 -0500

    Added print out of tweets

--- tweet.rb
+++ tweet.rb
@@ -10,10 +10,13 @@ class Tweet
 		sentiment_text = tweet_text.map {|text| URI.escape(text, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}
 
 		sentiment_response = sentiment_text.map {|text| HTTParty.get ("https://www.tweetsentimentapi.com/api/?key=f96b60fbda0c227488d4544ff70d02297f0f775c&text=#{text}")}
-		if sentiment_response[0] = "positive"
-			print tweet_text 
-		end
+
+		# print sentiment_response
+
+		print "#{sentiment_response.count {|response| response["sentiment"] = "positive"}} positive tweets were found that mentioned #{search_text}. \n"
 		
+		print "twitter user  |  tweet link  | twitter message \n"
+		print search_result.parsed_response["statuses"].map {|tweet| tweet["text"]}
 	end
 
 end
